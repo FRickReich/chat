1a2d8055f48c3f927a1aaae6cf98e9172d65098d
FRickReich <frickreich@gmail.com>
2021-10-05 14:22:44 +0200

    Add .gitignore and .env file

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+require('dotenv').config();
 
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) =>
+{
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
+io.on('connection', (socket) =>
+{
+    socket.on('chat message', (msg) =>
+    {
         io.emit('chat message', msg);
-      console.log('message: ' + msg);
+        console.log('message: ' + msg);
     });
-  });
+});
 
-server.listen(5000, () => {
-  console.log('listening on *:5000');
+server.listen(process.env.NODE_PORT, () =>
+{
+    console.log('listening on *:5000');
 });
 
